c8bf52ff8571842fc03fe920721fa3a33dc4ccd3
raiden101 <prabhusachin44@gmail.com>
2019-03-24 11:53:40 +0530

    blur edge condition handled

--- assets/scripts/workers/blur.js
+++ assets/scripts/workers/blur.js
@@ -12,16 +12,22 @@ this.onmessage = function({ data: { imgData, size } }) {
   const pixels = newImgData.data;
   const originalPixels = imgData.data;
   const s = size >> 1;
-  let i, _r, _g, _b;
+  let i, _r, _g, _b, copyRow, copyCol;
 
-  for(let row=s;row<(height-s);++row) {
-    for(let col=s;col<(width-s);++col) {
+  for(let row=0;row<(height);++row) {
+    for(let col=0;col<(width);++col) {
       i = getPixelPos(col, row, width);
 
       _r = 0;_g = 0;_b = 0;
       for(let r=row-s, m = 0;r<=(row+s);++r, ++m)
         for(let c=col-s, n = 0;c<=(col+s);++c, ++n) {
-          temp = getPixelPos(c, r, width);
+          copyRow = r;
+          copyCol = c;
+          if(copyRow < 0) copyRow = 0;
+          if(copyCol < 0) copyCol = 0;
+          if(copyRow > (height-1)) copyRow = height-1;
+          if(copyCol > (width-1)) copyCol = width-1;
+          temp = getPixelPos(copyCol, copyRow, width);
           _r += originalPixels[temp];
           _g += originalPixels[temp+1];
           _b += originalPixels[temp+2];
